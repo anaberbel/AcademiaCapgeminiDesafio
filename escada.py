77aefa521fd894ffea72a982a9b0b402f3a3c208
Ana Berbel <90399373+anaberbel@users.noreply.github.com>
2022-02-20 19:46:04 -0300

    Delete escada.py

--- escada.py
+++
@@ -1,9 +0,0 @@
-def construir(numero):
-    cont = 1
-    while cont <= numero:
-        print(' ' * (numero - cont) + '*' * cont)
-        cont += 1
-
-
-quantidade = int(input())
-construir(quantidade)
